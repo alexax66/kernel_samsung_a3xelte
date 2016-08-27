2f075dbe3ce175d21934a7b4449d681fddec4b54
alexax66 <alexax66@gmail.com>
2016-08-27 20:40:13 +0000

    build_kernel.sh: Add selection to TWRP variant

--- build_kernel.sh
+++ build_kernel.sh
@@ -3,12 +3,26 @@
 export ARCH=arm64
 #export CROSS_COMPILE=/opt/toolchains/aarch64-gnu-4.9/bin/aarch64-
 
+case "$1" in
+	a3)
+	    VARIANT="alexax"
+	    ;;
+
+	twrp)
+	    VARIANT="twrp"
+	    ;;
+
+	*)
+	    VARIANT="alexax"
+esac
+
+
 if [ ! -d $(pwd)/output ];
     then
         mkdir $(pwd)/output;
     fi
 
-make -C $(pwd) O=output ARCH=arm64 alexax_a3xelte_defconfig
+make -C $(pwd) O=output ARCH=arm64 $VARIANT"_a3xelte_defconfig"
 make -j5 -C $(pwd) O=output ARCH=arm64
 
 $(pwd)/dtbtool  -o $(pwd)/output/arch/arm64/boot/dt.img -s 2048 -p $(pwd)/output/scripts/dtc/ $(pwd)/output/arch/arm64/boot/dts/ -v
