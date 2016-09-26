d1dc84744f7a57e92e2f8d72ad516a5a6e30f5c2
Ard Biesheuvel <ard.biesheuvel@linaro.org>
2016-09-26 08:11:05 +0000

    arm64: add helper functions to read I-cache attributes
    This adds helper functions and #defines to <asm/cachetype.h> to read the
    line size and the number of sets from the level 1 instruction cache.
    
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Acked-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    
    Original commit: https://github.com/powervm/ibmvscsis/commit/80c517b0ff71a4c874fed9196fd990d2d9e911f3

--- arch/arm64/kernel/cpuinfo.c
+++ arch/arm64/kernel/cpuinfo.c
@@ -21,8 +21,10 @@
 #include <asm/cpufeature.h>
 
 #include <linux/bitops.h>
+#include <linux/bug.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
+#include <linux/preempt.h>
 #include <linux/printk.h>
 #include <linux/smp.h>
 
@@ -335,4 +337,15 @@ void __init cpuinfo_store_boot_cpu(void)
 	boot_cpu_data = *info;
 }
 
+u64 __attribute_const__ icache_get_ccsidr(void)
+{
+	u64 ccsidr;
+
+	WARN_ON(preemptible());
+
+	/* Select L1 I-cache and read its size ID register */
+	asm("msr csselr_el1, %1; isb; mrs %0, ccsidr_el1"
+	    : "=r"(ccsidr) : "r"(1L));
+	return ccsidr;
+}
 device_initcall(cpuinfo_regs_init);
