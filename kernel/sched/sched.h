0fbcae8cedc021c7883cb65d3e2d23e9223276d9
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2017-01-25 11:47:30 +0000

    sched: Fix bug in average nr_running and nr_iowait calculation
    sched_get_nr_running_avg() returns average nr_running and nr_iowait
    task count since it was last invoked. Fix several bugs in their
    calculation.
    
    * sched_update_nr_prod() needs to consider that nr_running count can
      change by more than 1 when CFS_BANDWIDTH feature is used
    
    * sched_get_nr_running_avg() needs to sum up nr_iowait count across
      all cpus, rather than just one
    
    * sched_get_nr_running_avg() could race with sched_update_nr_prod(),
      as a result of which it could use curr_time which is behind a cpu's
      'last_time' value. That would lead to erroneous calculation of
      average nr_running or nr_iowait.
    
    While at it, fix also a bug in BUG_ON() check in
    sched_update_nr_prod() function and remove unnecessary nr_running
    argument to sched_update_nr_prod() function.
    
    Used commit: https://github.com/jollaman999/jolla-kernel_bullhead/commit/3f116af70d390522ce0fa5c9db35befaefcdaa7c
    
    Change-Id: I46737614737292fae0d7204c4648fb9b862f65b2
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1128,7 +1128,7 @@ static inline unsigned int do_avg_nr_running(struct rq *rq)
 static inline void inc_nr_running(struct rq *rq)
 {
 	write_seqcount_begin(&rq->ave_seqcnt);
-	sched_update_nr_prod(cpu_of(rq), rq->nr_running, true);
+	sched_update_nr_prod(cpu_of(rq), 1, true);
 	rq->ave_nr_running = do_avg_nr_running(rq);
 	rq->nr_last_stamp = rq->clock_task;
 	rq->nr_running++;
@@ -1148,7 +1148,7 @@ static inline void inc_nr_running(struct rq *rq)
 static inline void dec_nr_running(struct rq *rq)
 {
 	write_seqcount_begin(&rq->ave_seqcnt);
-	sched_update_nr_prod(cpu_of(rq), rq->nr_running, false);
+	sched_update_nr_prod(cpu_of(rq), 1, false);
 	rq->ave_nr_running = do_avg_nr_running(rq);
 	rq->nr_last_stamp = rq->clock_task;
 	rq->nr_running--;
