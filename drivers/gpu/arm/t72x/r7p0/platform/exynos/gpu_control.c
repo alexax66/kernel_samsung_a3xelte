a2b4aedb0e2bfbdc07fbe339e17a05bb0b47f903
alexax66 <alexax66@mail.ru>
2017-02-12 17:43:18 +0000

    GPU-MALI-T720 r7p0: Set new min and max  GPU override freq

--- drivers/gpu/arm/t72x/r7p0/platform/exynos/gpu_control.c
+++ drivers/gpu/arm/t72x/r7p0/platform/exynos/gpu_control.c
@@ -27,8 +27,8 @@
 
 static struct gpu_control_ops *ctr_ops;
 
-unsigned int gpu_min_override = 266;
-unsigned int gpu_max_override = 772;
+unsigned int gpu_min_override = 160;
+unsigned int gpu_max_override = 800;
 unsigned int gpu_max_override_screen_off = 0;
 
 #ifdef CONFIG_MALI_RT_PM
