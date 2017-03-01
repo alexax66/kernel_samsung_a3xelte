3dfb98b49cbfdaee6fe94cab4e70fa209948579a
alexax66 <alexax66@mail.ru>
2017-03-01 21:09:13 +0000

    Revert "sdcardfs: Use per mount permissions"
    This reverts commit afec1df6c950205596e9760b7b307b2ee5a367d2.

--- fs/sdcardfs/sdcardfs.h
+++ fs/sdcardfs/sdcardfs.h
@@ -210,14 +210,6 @@ struct sdcardfs_mount_options {
 	type_t type;
 };
 
-struct sdcardfs_vfsmount_options {
-	gid_t gid;
-	mode_t mask;
-};
-
-extern int parse_options_remount(struct super_block *sb, char *options, int silent,
-		struct sdcardfs_vfsmount_options *vfsopts);
-
 /* sdcardfs super-block data in memory */
 struct sdcardfs_sb_info {
 	struct super_block *lower_sb;
