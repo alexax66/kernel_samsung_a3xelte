ffdb1792fc34e0908e56ff49c25856c211f61597
Lianwei Wang <a22439@motorola.com>
2017-03-27 19:14:20 +0000

    smp: make wake up idle cpus more generic
    wake_up_all_idle_cpus always wake up all the online cpus, but sometime
    we only want to wake up a set of cpus.
    
    Make the API more generic and wake up a set of idle cpus specified by
    the cpumask parameter.
    
    Change-Id: Idd1890a941106ff8b9a57a3167331f25f090070f
    Signed-off-by: Lianwei Wang <a22439@motorola.com>
    Reviewed-on: http://gerrit.mot.com/832905
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Zhi-Ming Yuan <a14194@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    
    Based on commit: https://github.com/robcore/android_kernel_samsung_msm8996/commit/7f238e3b4e187a57b80a9eacccfe64c0051797e8

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -209,7 +209,7 @@ void cpuidle_uninstall_idle_handler(void)
 {
 	if (enabled_devices) {
 		initialized = 0;
-		kick_all_cpus_sync();
+		wake_up_idle_cpus(cpu_online_mask);
 	}
 }
 
@@ -569,7 +569,7 @@ static void smp_callback(void *v)
 static int cpuidle_latency_notify(struct notifier_block *b,
 		unsigned long l, void *v)
 {
-	smp_call_function(smp_callback, NULL, 1);
+	wake_up_idle_cpus((struct cpumask *)v);
 	return NOTIFY_OK;
 }
 
