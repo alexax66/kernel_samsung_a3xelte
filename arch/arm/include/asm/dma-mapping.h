f404ca732611fca400399a64d668200db0fc5379
Santosh Shilimkar <santosh.shilimkar@ti.com>
2017-04-01 22:27:08 +0000

    ARM: 7805/1: mm: change max*pfn to include the physical offset of memory
    Most of the kernel code assumes that max*pfn is maximum pfns because
    the physical start of memory is expected to be PFN0. Since this
    assumption is not true on ARM architectures, the meaning of max*pfn
    is number of memory pages. This is done to keep drivers happy which
    are making use of of these variable to calculate the dma bounce limit
    using dma_mask.
    
    Now since we have a architecture override possibility for DMAable
    maximum pfns, lets make meaning of max*pfns as maximum pnfs on ARM
    as well.
    
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Cc: Nicolas Pitre <nicolas.pitre@linaro.org>
    Signed-off-by: Santosh Shilimkar <santosh.shilimkar@ti.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    
    Used commit: https://github.com/hizukiayaka/linux-kernel/commit/26ba47b18318abe7dadbe9294a611c0e932651d8

--- arch/arm/include/asm/dma-mapping.h
+++ arch/arm/include/asm/dma-mapping.h
@@ -104,6 +104,13 @@ static inline dma_addr_t virt_to_dma(struct device *dev, void *addr)
 }
 #endif
 
+/* The ARM override for dma_max_pfn() */
+static inline unsigned long dma_max_pfn(struct device *dev)
+{
+	return PHYS_PFN_OFFSET + dma_to_pfn(dev, *dev->dma_mask);
+}
+#define dma_max_pfn(dev) dma_max_pfn(dev)
+
 /*
  * DMA errors are defined by all-bits-set in the DMA address.
  */
