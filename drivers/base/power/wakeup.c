a8a4c4b2c6f31132ec01ad1bdfe189737dec8c37
Francisco Franco <franciscofranco.1990@gmail.com>
2017-04-14 19:34:10 +0000

    drivers: wakeup: be more thorough with blocking wakelocks
    This patch has been suggested by dorimanx https://forum.xda-developers.com/oneplus-3t/development/kernel-franco-kernel-r1-12nd-december-t3517519/post71609905
    
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Signed-off-by: engstk <eng.stk@sapo.pt>
    
    Based on commits:
     https://github.com/engstk/op3t/commit/36e5e1c230b437e1e14ceacbe4932f3e5e1af356
     https://github.com/engstk/op3t/commit/6bfcaaebfe0c5004dbd4a25a40872e0f98466d50
     https://github.com/engstk/op3t/commit/262662a55c36d13b53001c4576a20106f60a34a8
     https://github.com/engstk/op3t/commit/91640cc12f5aea50df85c4e70ec6f5eb0db5a405

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -525,7 +525,7 @@ static bool wakeup_source_blocker(struct wakeup_source *ws)
 {
 	unsigned int wslen = 0;
 
-	if (ws && ws->active) {
+	if (ws) {
 		wslen = strlen(ws->name);
 
 		if ((!enable_ipa_ws && !strncmp(ws->name, "IPA_WS", wslen)) ||
@@ -539,8 +539,12 @@ static bool wakeup_source_blocker(struct wakeup_source *ws)
 				!strncmp(ws->name, "[timerfd]", wslen)) ||
 			(!enable_netlink_ws &&
 				!strncmp(ws->name, "NETLINK", wslen))) {
-			wakeup_source_deactivate(ws);
-			pr_info("forcefully deactivate wakeup source: %s\n", ws->name);
+			if (ws->active) {
+				wakeup_source_deactivate(ws);
+				pr_info("forcefully deactivate wakeup source: %s\n",
+					ws->name);
+			}
+
 			return true;
 		}
 	}
@@ -592,9 +596,6 @@ static void wakeup_source_activate(struct wakeup_source *ws)
 			"unregistered wakeup source\n"))
 		return;
 
-	if (wakeup_source_blocker(ws))
-		return;
-
 	/*
 	 * active wakeup source should bring the system
 	 * out of PM_SUSPEND_FREEZE state
@@ -624,13 +625,15 @@ static void wakeup_source_activate(struct wakeup_source *ws)
  */
 static void wakeup_source_report_event(struct wakeup_source *ws)
 {
-	ws->event_count++;
-	/* This is racy, but the counter is approximate anyway. */
-	if (events_check_enabled)
-		ws->wakeup_count++;
+	if (!wakeup_source_blocker(ws)) {
+		ws->event_count++;
+		/* This is racy, but the counter is approximate anyway. */
+		if (events_check_enabled)
+			ws->wakeup_count++;
 
-	if (!ws->active)
-		wakeup_source_activate(ws);
+		if (!ws->active)
+			wakeup_source_activate(ws);
+	}
 }
 
 /**
