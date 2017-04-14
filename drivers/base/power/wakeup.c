311a79aa725377d423d0ca65ee680615b107f57b
Francisco Franco <franciscofranco.1990@gmail.com>
2017-04-14 19:30:44 +0000

    drivers: wakeup: run the wakelock blockers during wakeup_source activation and every resume
    Clean up the implementation to make it usable in more than one function.
    Run it every resume to make sure the wakeup source isn't still alive and kicking.
    
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Signed-off-by: engstk <eng.stk@sapo.pt>
    
    Used commit: https://github.com/engstk/op3t/commit/ba5fe540e3676d89fbc32b2b9b3e168a2956ab17

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -521,6 +521,33 @@ static bool wakeup_source_not_registered(struct wakeup_source *ws)
 		   ws->timer.data != (unsigned long)ws;
 }
 
+static bool wakeup_source_blocker(struct wakeup_source *ws)
+{
+	unsigned int wslen = 0;
+
+	if (ws && ws->active) {
+		wslen = strlen(ws->name);
+
+		if ((!enable_ipa_ws && !strncmp(ws->name, "IPA_WS", wslen)) ||
+			(!enable_wlan_extscan_wl_ws &&
+				!strncmp(ws->name, "wlan_extscan_wl", wslen)) ||
+			(!enable_qcom_rx_wakelock_ws &&
+				!strncmp(ws->name, "qcom_rx_wakelock", wslen)) ||
+			(!enable_wlan_ws &&
+				!strncmp(ws->name, "wlan", wslen)) ||
+			(!enable_timerfd_ws &&
+				!strncmp(ws->name, "[timerfd]", wslen)) ||
+			(!enable_netlink_ws &&
+				!strncmp(ws->name, "NETLINK", wslen))) {
+			wakeup_source_deactivate(ws);
+			pr_info("forcefully deactivate wakeup source: %s\n", ws->name);
+			return true;
+		}
+	}
+
+	return false;
+}
+
 /*
  * The functions below use the observation that each wakeup event starts a
  * period in which the system should not be suspended.  The moment this period
@@ -563,6 +590,9 @@ static void wakeup_source_activate(struct wakeup_source *ws)
 
 	if (WARN(wakeup_source_not_registered(ws),
 			"unregistered wakeup source\n"))
+		return;
+
+	if (wakeup_source_blocker(ws))
 		return;
 
 	/*
@@ -820,26 +850,10 @@ static void print_active_wakeup_sources(void)
 	rcu_read_lock();
 	list_for_each_entry_rcu(ws, &wakeup_sources, entry) {
 		if (ws->active) {
-			int wslen = strlen(ws->name);
-
 			pr_info("active wakeup source: %s\n", ws->name);
 
-			if ((!enable_ipa_ws && !strncmp(ws->name, "IPA_WS", wslen)) ||
-				(!enable_wlan_extscan_wl_ws &&
-					!strncmp(ws->name, "wlan_extscan_wl", wslen)) ||
-				(!enable_qcom_rx_wakelock_ws &&
-					!strncmp(ws->name, "qcom_rx_wakelock", wslen)) ||
-				(!enable_wlan_ws &&
-					!strncmp(ws->name, "wlan", wslen)) ||
-				(!enable_timerfd_ws &&
-					!strncmp(ws->name, "[timerfd]", wslen)) ||
-				(!enable_netlink_ws &&
-					!strncmp(ws->name, "NETLINK", wslen))) {
-				wakeup_source_deactivate(ws);
-				pr_info("forcefully deactivate wakeup source: %s\n", ws->name);
-			} else {
+			if (!wakeup_source_blocker(ws))
 				active = 1;
-			}
 		} else if (!active &&
 			   (!last_activity_ws ||
 			    ktime_to_ns(ws->last_time) >
