785729c8ad0a896d34aa21ffda5dbbecc3dc1570
Park Ju Hyung <qkrwngud825@gmail.com>
2017-06-11 08:36:09 +0000

    Add cafactive, version of interactive by CodeAurora
    Based on commits:
     https://github.com/jjhitel/ASKernel/commit/9984b242a05dbe0b8b25e2fdca49cde1d0a15eff
     https://github.com/jjhitel/ASKernel/commit/8e90d7919841925e013ab0e116f10acca074c282
     https://github.com/jjhitel/ASKernel/commit/0b25cc5b4f8a61a0e577a110e08ba0b62aad7e9f
     https://github.com/jjhitel/ASKernel/commit/1ccd77d010c7403b0c8019051e39d8be54fc71d7
     https://github.com/jjhitel/ASKernel/commit/7c642a36151e1840926827eccdcae0aed01b41e6
     https://github.com/jjhitel/ASKernel/commit/b93b5615ae5b7ffc2002f403f76ba5ebe221c72d
     https://github.com/jjhitel/ASKernel/commit/249aacfd76a425bad6e153172378114bd0b4c22e
     https://github.com/jjhitel/ASKernel/commit/93cf4754c13bb17840aa374839e3d6d620bac800
     https://github.com/jjhitel/ASKernel/commit/8f90aa765a0af64f75ca812a4dfa2a9fe1e6a714
     https://github.com/jjhitel/ASKernel/commit/289e6729c84b3be8088826ff05020a98b54fa29f
     https://github.com/jjhitel/ASKernel/commit/27219e15647f3f98c340d93aa4c1ec011290e22b
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Derek Basehore <dbasehore@chromium.org>
    
    Adapded by: Alexander Alexeev <alexax66@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -429,6 +429,9 @@ extern struct cpufreq_governor cpufreq_gov_interextrem;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT__GOV_CHILL)
 extern struct cpufreq_governor cpufreq_gov_chill;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_chill)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CAFACTIVE)
+extern struct cpufreq_governor cpufreq_gov_cafactive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_cafactive)
 #endif
 
 
